5dcb247e23bc8510c316aaf23b83f38a5ddb162d
Orange Squirter <orangesquirter@gmail.com>
2022-12-09 23:53:36 +0000

    set up user enumeration playbook/ filtering

---
+++ playbooks/userenum/userenum.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ListOfTargets=/opt/redteam-ansible/inventory/teams
+Lines=$(cat $ListOfTargets)
+for Line in $Lines;
+do	
+	ansible-playbook /opt/redteam-ansible/playbooks/userenum/userenum.yaml -i /opt/redteam-ansible/inventory/hosts --extra-vars "variable_hosts=${Line}" >> /opt/redteam-ansible/inventory/users/${Line}.tmp;
+done;
+# Filter out anything that is not a username
+for Line in $Lines;
+do
+	grep -ho 'home/[^" ]\+' /opt/redteam-ansible/inventory/users/${Line}.tmp | cut -d- -f2 | sort -u >> /opt/redteam-ansible/inventory/users/${Line};
+	sed -i -e 's/home\///g' /opt/redteam-ansible/inventory/users/${Line}
+	awk -i inplace '!seen[$0]++' /opt/redteam-ansible/inventory/users/${Line}
+done
