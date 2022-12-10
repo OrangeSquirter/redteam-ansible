bc0dd7c42cf2914b8ae6a90d6f64ee74ea931efe
Orange Squirter <orangesquirter@gmail.com>
2022-12-10 02:22:32 +0000

    Change scripts for moved files

--- playbooks/usercheck/usercheck.sh
+++ playbooks/usercheck/usercheck.sh
@@ -4,10 +4,11 @@ ListOfTargets=/opt/redteam-ansible/inventory/teams
 Lines=$(cat $ListOfTargets)
 for Line in $Lines;
 do
-	Users=$(cat /opt/redteam-ansible/inventory/users/${Line})
+	Users=$(cat /opt/redteam-ansible/inventory/users/${Line}/${Line})
 	for User in $Users;
 	do
 		temp="$(echo "$User"|tr -d '\n')"
+		temp="$(echo "$temp"|tr -d \'\")"
 		ansible-playbook /opt/redteam-ansible/playbooks/usercheck/usercheck.yaml -i /opt/redteam-ansible/inventory/hosts --ask-become-pass --extra-vars "variable_hosts=${Line} variable_user=${temp}";
 	done;
 done;
--- playbooks/userenum/userenum.sh
+++ playbooks/userenum/userenum.sh
@@ -4,12 +4,12 @@ ListOfTargets=/opt/redteam-ansible/inventory/teams
 Lines=$(cat $ListOfTargets)
 for Line in $Lines;
 do	
-	ansible-playbook /opt/redteam-ansible/playbooks/userenum/userenum.yaml -i /opt/redteam-ansible/inventory/hosts --extra-vars "variable_hosts=${Line}" >> /opt/redteam-ansible/inventory/users/${Line}.tmp;
+	ansible-playbook /opt/redteam-ansible/playbooks/userenum/userenum.yaml -i /opt/redteam-ansible/inventory/hosts --extra-vars "variable_hosts=${Line}" >> /opt/redteam-ansible/inventory/users/${Line}/${Line}.tmp;
 done;
 # Filter out anything that is not a username
 for Line in $Lines;
 do
-	grep -ho 'home/[^" ]\+' /opt/redteam-ansible/inventory/users/${Line}.tmp | cut -d- -f2 | sort -u >> /opt/redteam-ansible/inventory/users/${Line};
-	sed -i -e 's/home\///g' /opt/redteam-ansible/inventory/users/${Line}
-	awk -i inplace '!seen[$0]++' /opt/redteam-ansible/inventory/users/${Line}
+	grep -ho 'home/[^" ]\+' /opt/redteam-ansible/inventory/users/${Line}/${Line}.tmp | cut -d- -f2 | sort -u >> /opt/redteam-ansible/inventory/users/${Line}/${Line};
+	sed -i -e 's/home\///g' /opt/redteam-ansible/inventory/users/${Line}/${Line}
+	awk -i inplace '!seen[$0]++' /opt/redteam-ansible/inventory/users/${Line}/${Line}
 done
